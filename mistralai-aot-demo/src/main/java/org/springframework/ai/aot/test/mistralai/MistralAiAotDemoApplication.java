31b38ee7f0e12609b6d0e4ccd173063e5cdfe325
Christian Tzolov <ctzolov@vmware.com>
2024-03-03 18:00:15 +0100

    Add Mistral Function Calling to the demo

--- mistralai-aot-demo/src/main/java/org/springframework/ai/aot/test/mistralai/MistralAiAotDemoApplication.java
+++ mistralai-aot-demo/src/main/java/org/springframework/ai/aot/test/mistralai/MistralAiAotDemoApplication.java
@@ -1,20 +1,19 @@
 package org.springframework.ai.aot.test.mistralai;
 
 import java.util.List;
+import java.util.Map;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
-import com.fasterxml.jackson.annotation.JsonClassDescription;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 import reactor.core.publisher.Flux;
 
 import org.springframework.ai.chat.ChatResponse;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.mistralai.MistralAiChatClient;
+import org.springframework.ai.mistralai.MistralAiChatOptions;
 import org.springframework.ai.mistralai.MistralAiEmbeddingClient;
+import org.springframework.ai.mistralai.api.MistralAiApi;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
@@ -25,9 +24,11 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Description;
 import org.springframework.context.annotation.ImportRuntimeHints;
+import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 
 @SpringBootApplication
-@ImportRuntimeHints(MistralAiAotDemoApplication.MockWeatherServiceRuntimeHints.class)
+@ImportRuntimeHints(MistralAiAotDemoApplication.PaymentStatusServiceRuntimeHints.class)
 public class MistralAiAotDemoApplication {
 
     public static void main(String[] args) {
@@ -39,8 +40,6 @@ public class MistralAiAotDemoApplication {
     @Bean
     ApplicationRunner applicationRunner(MistralAiChatClient chatClient, MistralAiEmbeddingClient embeddingClient) {
         return args -> {
-            System.out.println("ChatClient: " + chatClient.getClass().getName());
-            System.out.println("EmbeddingClient: " + embeddingClient.getClass().getName());
 
             // Synchronous Chat Client
             String response = chatClient.call("Tell me a joke.");
@@ -56,57 +55,46 @@ public class MistralAiAotDemoApplication {
             List<List<Double>> embeddings = embeddingClient.embed(List.of("Hello", "World"));
             System.out.println("EMBEDDINGS SIZE: " + embeddings.size());
 
-            // Function calling
-            // ChatResponse weatherResponse = chatClient.call(new Prompt("What is the weather in Amsterdam, Netherlands?",
-            //         MistralAiChatOptions.builder().withFunction("weatherInfo").build()));
-            // System.out.println("WEATHER RESPONSE: " + weatherResponse.getResult().getOutput().getContent());
+            // Function calling.
+            // Function name is the bean name. Only the Mistral AI large and small models support function calling.
+            ChatResponse paymentStatusResponse = chatClient
+                    .call(new Prompt("What's the status of my transaction with id T1005?",
+                            MistralAiChatOptions.builder()
+                                    .withModel(MistralAiApi.ChatModel.SMALL.getValue())
+                                    .withFunction("retrievePaymentStatus").build()));
+
+            System.out.println("PAYMENT STATUS: " + paymentStatusResponse.getResult().getOutput().getContent());
         };
     }
 
+    public record Transaction(@JsonProperty(required = true, value = "transaction_id") String transactionId) {
+    }
+
+    public record Status(@JsonProperty(required = true, value = "status") String status) {
+    }
+
     @Bean
-    @Description("Get the weather in location")
-    public Function<MockWeatherService.Request, MockWeatherService.Response> weatherInfo() {
-        return new MockWeatherService();
+    @Description("Get payment status of a transaction")
+    public Function<Transaction, Status> retrievePaymentStatus() {
+        return (transaction) -> new Status(PAYMENT_DATA.get(transaction).status());
     }
 
-    public static class MockWeatherService
-            implements Function<MockWeatherService.Request, MockWeatherService.Response> {
+    // Assuming we have the following data
+    public static final Map<Transaction, Status> PAYMENT_DATA = Map.of(
+            new Transaction("T1001"), new Status("Paid"),
+            new Transaction("T1002"), new Status("Unpaid"),
+            new Transaction("T1003"), new Status("Paid"),
+            new Transaction("T1004"), new Status("Paid"),
+            new Transaction("T1005"), new Status("Pending"));
 
-        @JsonInclude(Include.NON_NULL)
-        @JsonClassDescription("Weather API request")
-        public record Request(
-                @JsonProperty(required = true, value = "location") @JsonPropertyDescription("The city and state e.g. San Francisco, CA") String location,
-                @JsonProperty(required = true, value = "lat") @JsonPropertyDescription("The city latitude") double lat,
-                @JsonProperty(required = true, value = "lon") @JsonPropertyDescription("The city longitude") double lon,
-                @JsonProperty(required = true, value = "unit") @JsonPropertyDescription("Temperature unit") Unit unit) {
-        }
-
-        public enum Unit {
-            C, F;
-        }
-
-        @JsonInclude(Include.NON_NULL)
-        public record Response(double temperature, double feels_like, double temp_min, double temp_max, int pressure,
-                int humidity, Unit unit) {
-        }
-
+    // GraalVM Reflection Configuration
+    public static class PaymentStatusServiceRuntimeHints implements RuntimeHintsRegistrar {
         @Override
-        public Response apply(Request request) {
-            System.out.println("Weather request: " + request);
-            return new Response(11, 15, 20, 2, 53, 45, Unit.C);
-        }
-
-    }
-
-    public static class MockWeatherServiceRuntimeHints implements RuntimeHintsRegistrar {
-        @Override
-        public void registerHints(RuntimeHints hints, ClassLoader classLoader) {
+        public void registerHints(@NonNull RuntimeHints hints, @Nullable ClassLoader classLoader) {
             // Register method for reflection
             var mcs = MemberCategory.values();
-            hints.reflection().registerType(MockWeatherService.Request.class, mcs);
-            hints.reflection().registerType(MockWeatherService.Response.class, mcs);
+            hints.reflection().registerType(Transaction.class, mcs);
+            hints.reflection().registerType(Status.class, mcs);
         }
-
     }
-
 }
